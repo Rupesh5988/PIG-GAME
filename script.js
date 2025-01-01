9831a2b1ac4b162f0480fec5d8d36c59e9caba55
Rupesh Omprakash Honrao <149290951+Rupesh5988@users.noreply.github.com>
2025-01-01 14:40:28 +0530

    Update script.js
    just added toggle

--- script.js
+++ script.js
@@ -1,68 +1,46 @@
-var scores,activePlayer,roundScore,dice;
-scores=[0,0];
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
+
+
+
+var score,currentScore,activePlayer,randomScore;
+function newGame(){
+score=[0,0];
+currentScore=0;
 activePlayer=0;
-roundScore=0;
-
-document.getElementById('score--0').textContent=0;
-document.getElementById('score--1').textContent=0;
-document.getElementById('current--0').textContent=0;
-document.getElementById('current--1').textContent=0;
-
-document.querySelector('.dice').style.display='none';
-
- document.querySelector('.btn--roll').addEventListener('click', function() {
-    var player0=document.querySelector('#score--0');
-    var player1=document.querySelector('#score--1');
-    
-   var dice=Math.floor(Math.random()*6 + 1);
-    var diceDOM=document.querySelector('.dice');
-    diceDOM.style.display='block';
-    diceDOM.src='dice-'+dice+'.png';
-    
-   if(dice>1){
-      roundScore+=dice;
-      document.querySelector('#current--'+activePlayer).textContent=roundScore;
-   }
-   else{
+randomScore=0;
+document.querySelector('#score--0').textContent=0;
+document.querySelector('#score--1').textContent=0;
+document.querySelector('#current--0').textContent=0;
+document.querySelector('#current--1').textContent=0;
+document.querySelector('.dice').style.display='none';}
+newGame();
+function nextPlayer(){
+    activePlayer=((activePlayer+1)%2);  
+    currentScore=0;
+    document.querySelector('.dice').style.display='none';
+    player0El.classList.toggle('player--active');
+  player1El.classList.toggle('player--active');
+}
+document.querySelector('.btn--roll').addEventListener('click',function(){
+var diceNumber=Math.floor((Math.random()*6) +1);
+document.querySelector('.dice').src='dice-'+diceNumber+'.png';
+document.querySelector('.dice').style.display='block';
+if(diceNumber>1){
+    currentScore+=diceNumber;
+document.querySelector('#current--'+activePlayer).textContent=currentScore;
+}
+else{
+    document.querySelector('#current--'+activePlayer).textContent=0;
     document.querySelector('#score--'+activePlayer).textContent=0;
-    
-      nextPlayer();
+    nextPlayer();
+} })
 
-   }
-   
-
- });    
  document.querySelector('.btn--hold').addEventListener('click',function(){
-   scores[activePlayer]+=roundScore;
-document.querySelector('#score--'+activePlayer).textContent=scores[activePlayer];
-if(scores[activePlayer]>=20 ){
-   document.querySelector('#name--'+activePlayer).textContent='YOU WON';
-}else{nextPlayer();}
-
-
- })
- function nextPlayer(){
-   document.querySelector('#current--'+activePlayer).textContent=0;
-
-   roundScore=0;
-   var diceDOM=document.querySelector('.dice');
-   
-   diceDOM.style.display='none';
-   activePlayer = (activePlayer + 1) % 2;
-   document.querySelector('.player--0').classList.toggle('active');
-   document.querySelector('.player--1').classList.toggle('active');
-   
- }
- document.querySelector('.btn--new').addEventListener('click',function(){
-   scores=[0,0];
-activePlayer=0;
-roundScore=0;
-
-document.getElementById('score--0').textContent=0;
-document.getElementById('score--1').textContent=0;
-document.getElementById('current--0').textContent=0;
-document.getElementById('current--1').textContent=0;
-
-document.querySelector('.dice').style.display='none';
-
- })
+    score[activePlayer]+=currentScore;
+document.querySelector('#score--'+activePlayer).textContent=score[activePlayer];
+if(score[activePlayer]>=100){
+    document.querySelector('#name--'+activePlayer).textContent='YOU WON!'}
+else{ nextPlayer();}})
+document.querySelector('.btn--new').addEventListener('click',function(){
+newGame();})
